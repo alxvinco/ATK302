bb9c3e115f976e6871567d73e374304e674a69fc
alxvinco <vinco78@163.com>
2020-06-18 09:19:48 +0800

    Update sketch.js

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,29 @@
 function setup() {
-  createCanvas(500, 500);
-  
+
+  createCanvas(720, 400);
+  background(0);
+  noStroke();
+
+  fill(204);
+  triangle(18, 18, 18, 360, 81, 360);
+
+  fill(102);
+  rect(81, 81, 63, 63);
+
+  fill(204);
+  quad(189, 18, 216, 18, 216, 360, 144, 360);
+
+  fill(255);
+  ellipse(252, 144, 72, 72);
+
+  fill(204);
+  triangle(288, 18, 351, 360, 288, 360);
+
+  fill(255);
+  arc(479, 300, 280, 280, PI, TWO_PI);
 }
 
-function draw() {
-  background(100) ; 
-  // put your shapes here!
-  
-  
-  text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
-
-  
-
-function mouseReleased() { 
+function mouseReleased() {
   // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
-
- 
+  print(mouseX + " , " + mouseY) ;
+}
